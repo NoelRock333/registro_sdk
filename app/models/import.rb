b8b2a0a677912f829f8c80e0783c15aa7db7d18e
Noel Escobedo <noel@michelada.io>
2018-05-10 03:45:47 +0000

    modified fields

--- app/models/import.rb
+++ app/models/import.rb
@@ -10,15 +10,13 @@ class Import < ApplicationRecord
           :name => @row_hash['Nombres'],
           :last_name => @row_hash['Apellidos'],
           :email => @row_hash['Correo electrónico'],
-          :payment_status => @row_hash['Estado del pago'],
           :pay_method => @row_hash['Forma de pago'],
-          :tiket_quantity => @row_hash['Número de boletos'],
+          :ticket_quantity => @row_hash['Número de boletos'],
           :reservation_date => @row_hash['Fecha de creación dd/mm/aaaa']
         ]
         puts @assistent
+        Assistant.create! @assistent
       end
-      #Assistant.create()
-      #Import.create! row.to_hash
     end
   end
 end
